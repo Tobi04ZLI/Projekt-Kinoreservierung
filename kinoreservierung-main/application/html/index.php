74f5e78f4bbfb53594c10d32119004cb48cfeded
Tobias Bertschi <bertschi.tobias@bluewin.ch>
2021-02-08 15:43:56 +0100

    erstellen der Datenbankverknüpfung

--- kinoreservierung-main/application/html/index.php
+++ kinoreservierung-main/application/html/index.php
@@ -2,12 +2,13 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" type="image/png" sizes="32x32" href="./icons/video-camera.png">
         <title>Hello World!</title>
     </head>
     <body>
-    
+    Hello
     <?php
-    phpinfo();
+    
     ?>
     </body>
 </html>
