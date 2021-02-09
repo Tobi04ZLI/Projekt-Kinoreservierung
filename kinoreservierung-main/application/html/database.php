25c491c853a5868e98028e5f155c7be9d56ab31f
Maurin Schucan <maurin@schucan.ch>
2021-02-09 10:16:04 +0100

    changed db connection

--- kinoreservierung-main/application/html/database.php
+++ kinoreservierung-main/application/html/database.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "mariadb";
+$servername = "mariadatabase";
 $user = "root";
-$password = "happyplace";
-$dbname = "happyplace";
+$password = "kino";
+$dbname = "kinoreser";
 
 $connection = new mysqli($servername, $user, $password, $dbname);
 
--- kinoreservierung-main/application/html/room.class.php
+++ kinoreservierung-main/application/html/room.class.php
@@ -1,7 +1,16 @@
 <?php
 require_once ("database.php");
 
+class reserved{
 
+    
+
+    public function __constructor(){
+
+    }
+
+
+}
 
 
 
