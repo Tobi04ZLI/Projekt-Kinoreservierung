a3428f3c5ba56ab35d53402c8308d27f409cd1e0
Tobias Bertschi <bertschi.tobias@bluewin.ch>
2021-02-09 11:11:44 +0100

    hinzufügen von files

--- kinoreservierung-main/application/html/registerbesucher.php
+++ kinoreservierung-main/application/html/registerbesucher.php
@@ -1,37 +1,4 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-    <style>
-        .back {
-            margin-left: -10px;
-            margin-top: 41px;
-            background-color: red;
-            width: 177px;
-        }
-
-        .input {
-            margin-left: 830px;
-            margin-top: 250px;
-        }
-
-        .backtwo {
-            background-color: green;
-        }
-    </style>
-</head>
-
-<body>
-
-    <form action="registerbesucher.php" method="POST">
-        <div class="input">
-            <input type="text" name="username" placeholder="username"> <br> <br>
-            <input type="text" name="email" placeholder="email"> <br> <br>
-            <button type="submit" name="submit">
-                register
-            </button>
-        </div>
-    </form>
 
 
 <?php
@@ -71,6 +38,3 @@ class register{
     }
 }
 ?>
-
-</body>
-</html>
