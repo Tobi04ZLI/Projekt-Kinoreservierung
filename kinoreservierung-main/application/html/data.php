2f25d36ed8016ed3156614397f51c56b5a5fb404
Maurin Schucan <maurin@schucan.ch>
2021-02-10 16:25:38 +0100

    test

--- kinoreservierung-main/application/html/data.php
+++ kinoreservierung-main/application/html/data.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once 'registerbesucher.php';
+//require_once 'registerbesucher.php';
 require_once 'loginadmin.php';
 require_once 'database.php';
 
 
-$registirierung = new register($username, $email);
-$create = $registirierung->create($connection);
+//$registirierung = new register($username, $email);
+//$create = $registirierung->create($connection);
 
 $login = new Login($adminname, $adminpassword);
 $create2 = $login->login($connection);
