03f68fceca74b3387c61fa948e3c8ed799751623
Maurin Schucan <maurin@schucan.ch>
2021-02-10 16:46:54 +0100

    Status quo, everything is working

--- kinoreservierung-main/application/html/movie.class.php
+++ kinoreservierung-main/application/html/movie.class.php
@@ -15,59 +15,51 @@ class reserv{
     }
 
     public function reserved($connection){
-        
         if(isset($_POST["button"])){
             $username = $_POST['username'];
             $email = $_POST['email'];
             $seat = $_POST['seat'];
 
-            if ($username != "" && $email != "") {
-                $sql = "SELECT * FROM users";
-                $result = $connection->query($sql);
-                $usrsFromDatabase = $result->fetch_all(MYSQLI_ASSOC);
-                foreach ($usrsFromDatabase as $usrs) {
-                    ($usrs);
-                    $usrname = $usrs['username'];
-                    $mail = $usrs['email'];
-                    if($username != $usrname && $email != $mail){
-                        $query = "INSERT INTO users (username, email) VALUES ('$username', '$email');";
-                        $result = $connection->query($query);
-/*
-                        $sql = "SELECT usersid FROM users WHERE $username = 'username' AND $email = 'email';";
-                        $result = $connection->query($sql);
-*/
-                        if (!$result) {
-                            die($connection->error);
-                        }
-                        
-                        if ($connection->query($sql) === TRUE) {
-                            echo "New record created successfully";
-                        } else {
-                            echo "Error: " . $sql . "<br>" . $connection->error;
-                        }
-                        
-//                        $insert = "INSERT INTO cinemaroomone (usersid, seatid) VALUES ('$sql', '$seat');";
-
-                        $connection->close();
-                        header("Location: Movie1.php");
-                    } else {
-                        $sql = "SELECT usersid FROM users WHERE $username = 'username' AND $email = 'email';";
-                        $result = $connection->query($sql);
-
-                        if (!$result) {
-                            die($connection->error);
-                        }
-                        
-                        if ($connection->query($sql) === TRUE) {
-                            echo "New record created successfully";
-                        } else {
-                            echo "Error: " . $sql . "<br>" . $connection->error;
-                        }
-                        $query = "INSERT INTO cinemaroomone (usersid, seatid) VALUES ('$sql', '$seat');";
-
-                        $connection->close();
-                        header("Location: Movie1.php");
+            $sql = "SELECT * FROM users";
+            $result = $connection->query($sql);
+            $usrsFromDatabase = $result->fetch_all(MYSQLI_ASSOC);
+            foreach ($usrsFromDatabase as $usrs) {
+                ($usrs);
+                $usrname = $usrs['username'];
+                $mail = $usrs['email'];
+                if($username != $usrname && $email != $mail){
+                    $query = "INSERT INTO users (username, email) VALUES ('$username', '$email');";
+                    $result = $connection->query($query);
+                    if (!$result) {
+                        die($connection->error);
                     }
+                    
+                    if ($connection->query($sql) === TRUE) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $connection->error;
+                    }
+                    
+                    $insert = "INSERT INTO cinemaroomone (usersid, seatid) VALUES ('$sql', '$seat');";
+                    $connection->close();
+                    header("Location: Movie1.php");
+                }else {
+                    $sql = "SELECT usersid FROM users WHERE $username = 'username' AND $email = 'email';";
+                    $result = $connection->query($sql);
+        
+                    if (!$result) {
+                        die($connection->error);
+                    }
+                    
+                    if ($connection->query($sql) === TRUE) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $connection->error;
+                    }
+                    $query = "INSERT INTO cinemaroomone (usersid, seatid) VALUES ('$sql', '$seat');";
+        
+                    $connection->close();
+                    header("Location: Movie1.php");
                 }
             }
         }
