7603d8e0c05534d873db251acc2e97cb7965fce8
Tobias Bertschi <bertschi.tobias@bluewin.ch>
2021-02-11 09:35:42 +0100

    CSS movie.css finished

--- kinoreservierung-main/application/html/Movie1.php
+++ kinoreservierung-main/application/html/Movie1.php
@@ -1,8 +1,9 @@
 <?php
-require_once ("moviedata.php");
+require_once("moviedata.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,19 +12,31 @@ require_once ("moviedata.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <button class="login" onclick="location.href='index.php'">back to main page</button>
-    
-    <form action="Movie1.php" method="POST">
-        <div>
-            <label for="name">Name</label>
-            <label for="email">Email</label>
-            <label for="seat">Seat</label><br>
+
+    <form id="all" action="Movie1.php" method="POST">
+        <div id="one">
+            <label for="name">Name</label> <br>
             <input type="text" name="username">
-            <input type="text" name="email">
-            <input type="text" name="seat">
-            <button type="submit" name="button">Reserve</button>
         </div>
+
+        <div id="two">
+        <label for="email">Email</label> <br>
+        <input type="text" name="email">
+        </div>
+
+        <div id="three">
+        <label for="seat">Seat</label> <br>
+        <input type="text" name="seat">
+        </div>
+
+        <div id="four">
+        <button type="submit" name="button">Reserve</button>
+        </div>
+
     </form>
 </body>
+
 </html>
