3f8a74d4e3208160beb810ba5e435b954787d572
Tobias Bertschi <bertschi.tobias@bluewin.ch>
2021-02-11 15:10:07 +0100

    finished design login and register

--- kinoreservierung-main/application/html/adminlogin.php
+++ kinoreservierung-main/application/html/adminlogin.php
@@ -20,7 +20,7 @@ require_once 'data.php';
     <form action="adminlogin.php" method="POST">
         <div id="all">
             <input id="one" type="text" name="adminname" placeholder="username"> <br> <br>
-            <input id="two" type="password" name="adminpassword" placeholder="passwort"> <br> <br>
+            <input id="two" type="password" name="adminpassword" placeholder="password"> <br> <br>
             <button id="three" type="submit" name="submit">login</button>
         </div>
     </form>
